2c220d054bb3f39cf13e7aabc8f809b0f698fe38
刘炳宏 <liubinghong@keking.cn>
2018-10-16 12:01:35 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from datetime import datetime
+import re
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return 'Hello, Flask!'
+
+@app.route("/hello/name")
+def hello_there(name):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
